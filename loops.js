356a3f17550c16b75311615b290698952e19288c
Edward Hambleton <ehambleton@me.com>
2018-01-16 02:22:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,11 +16,12 @@ function whileLoop(n) {
 return "done"
 }
 
-function maybeTrue() {
-return Math.random() >= 0.5
-}
+
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
   do {
 array.shift(1)
 while (array.length > 0 && maybeTrue());
